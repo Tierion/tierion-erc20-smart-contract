b3845897a682941a55678196af4f2d4cd7bf2bd2
Glenn Rempe <glenn@tierion.com>
2017-08-15 13:56:27 -0700

    Correct typos carried from source. Bug filed: https://github.com/OpenZeppelin/zeppelin-solidity/issues/371

--- contracts/TierionNetworkToken.sol
+++ contracts/TierionNetworkToken.sol
@@ -54,7 +54,7 @@ contract TierionNetworkToken is StandardToken, Pausable {
    * @dev Transfer tokens from one address to another when not paused
    * @param _from address The address which you want to send tokens from
    * @param _to address The address which you want to transfer to
-   * @param _value uint256 the amout of tokens to be transfered
+   * @param _value uint256 the amount of tokens to be transferred
    */
   function transferFrom(address _from, address _to, uint256 _value) whenNotPaused returns (bool) {
     require(_to != address(0));
